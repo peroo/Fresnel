ed1485fefc32373905208ff85257e237931a12d3
Per Øyvind Øygard <peroo@Juliette.(none)>
2009-04-12 20:56:44 +0200

    Renamed Main->Slingshot

--- Slingshot.h
+++ Slingshot.h
@@ -1,19 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SLINGSHOT_H
+#define SLINGSHOT_H
 
 #include <boost/filesystem/path.hpp>
 
 #define PORT 9999
 
-
 struct sqlite3;
 struct MHD_Daemon;
 
-class Main {
+class Slingshot {
 	public:
-		Main(){};
-		~Main(){};
-
 		bool Initialize();
 		void StopServer();
 

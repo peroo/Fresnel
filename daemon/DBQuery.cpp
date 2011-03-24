c6e1349dc3445fa01a2e253e3e9174eb938936fc
Per Øyvind Øygard <peroyo@gmail.com>
2011-03-24 13:10:15 +0530

    Add XSPF playlist generation.

--- daemon/DBQuery.cpp
+++ daemon/DBQuery.cpp
@@ -1,6 +1,7 @@
 #include "DBQuery.h"
 #include "sqlite3.h"
 #include "HttpRequest.h"
+#include <xspf/Xspf.h>
 #include <iostream>
 #include <sstream>
 
@@ -30,6 +31,20 @@ void DBQuery::parse()
         else if(req->parameters[0] == "random") {
             fetchM3URandom();
         }
+    }
+    else if(req->object == "xspf") {
+       if(req->parameters[0] == "album") {
+           fetchXSPFAlbum(req->parameters[1]);
+       }
+       else if(req->parameters[0] == "artist"){
+           fetchXSPFArtist(req->parameters[1]);
+       }
+       else if(req->parameters[0] == "random") {
+           fetchXSPFRandom();
+       }
+       else if(req->parameters[0] == "date"){
+           fetchXSPFDate(req->parameters[1]);
+       }
     }
     else {
         req->fail(MHD_HTTP_NOT_FOUND);
@@ -108,6 +123,23 @@ void DBQuery::fetchM3UAlbum(std::string album)
     req->render(processM3U(), "audio/x-mpegurl");
 }
 
+void DBQuery::fetchXSPFAlbum(std::string album)
+{
+    query(
+        "SELECT R.id,T.title,I.name AS artist,A.title AS album,tracknumber, length \
+         FROM resource R \
+         JOIN audio_track T USING (id) \
+         JOIN audio_album A ON album=A.id \
+         JOIN artist I ON T.artist=I.id \
+         WHERE type=0 \
+         AND A.title LIKE ? \
+         ORDER BY A.title, R.path_id, tracknumber ASC");
+
+    bindString("%" + album + "%");
+
+    req->render(processXSPF(), "application/xspf+xml");
+}
+
 void DBQuery::fetchM3UArtist(std::string artist)
 {
     query(
@@ -125,6 +157,23 @@ void DBQuery::fetchM3UArtist(std::string artist)
     req->render(processM3U(), "audio/x-mpegurl");
 }
 
+void DBQuery::fetchXSPFArtist(std::string artist)
+{
+    query(
+        "SELECT R.id,T.title,I.name AS artist,A.title AS album,tracknumber, length \
+         FROM resource R \
+         JOIN audio_track T USING (id) \
+         JOIN audio_album A ON album=A.id \
+         JOIN artist I ON T.artist=I.id \
+         WHERE type=0 \
+         AND I.name LIKE ? \
+         ORDER BY A.date ASC, A.title, R.path_id, tracknumber ASC");
+
+    bindString("%" + artist + "%");
+
+    req->render(processXSPF(), "application/xspf+xml");
+}
+
 void DBQuery::fetchM3URandom()
 {
     query(
@@ -140,6 +189,37 @@ void DBQuery::fetchM3URandom()
     req->render(processM3U(), "audio/x-mpegurl");
 }
 
+void DBQuery::fetchXSPFRandom()
+{
+    query(
+        "SELECT R.id,T.title,I.name AS artist,A.title AS album,tracknumber, length \
+         FROM resource R \
+         JOIN audio_track T USING (id) \
+         JOIN audio_album A ON album=A.id \
+         JOIN artist I ON T.artist=I.id \
+         WHERE type=0 \
+         ORDER BY RANDOM() \
+         LIMIT 50");
+
+    req->render(processXSPF(), "application/xspf+xml");
+}
+
+void DBQuery::fetchXSPFDate(std::string date)
+{
+    query(
+        "SELECT R.id,T.title,I.name AS artist,A.title AS album,tracknumber, length \
+         FROM resource R \
+         JOIN audio_track T USING (id) \
+         JOIN audio_album A ON album=A.id \
+         JOIN artist I ON T.artist=I.id \
+         WHERE type=0 \
+         AND A.date LIKE ? \
+         ORDER BY A.date ASC, A.title, tracknumber ASC");
+
+    bindString("%" + date + "%");
+
+    req->render(processXSPF(), "application/xspf+xml");
+}
 
 std::string escapeStr(std::string str)
 {
@@ -170,18 +250,57 @@ std::string DBQuery::processM3U()
         int length = getInt();
 
         output << "#EXTINF:" << length << "," << artist << " - " << title << std::endl;
-        output << "http://129.241.122.110:9996/resource/" << id << "/" << tracknumber << ".ogg" <<  std::endl << std::endl;
+        output << "http://129.241.122.115:9996/resource/" << id << "/" << tracknumber << ".ogg" <<  std::endl << std::endl;
 
     } while(step());
     return output.str();
 
 }
 
+std::string DBQuery::processXSPF()
+{
+    bool res = step();
+
+    if(!res) {
+        return "error";
+    }
+
+    Xspf::XspfIndentFormatter formatter = Xspf::XspfIndentFormatter();
+    Xspf::XspfWriter *writer = Xspf::XspfWriter::makeWriter(formatter, NULL);
+
+    do {
+        int id = getInt();
+        std::string title = getString();
+        std::string artist = getString();
+        std::string album = getString();
+        int tracknumber = getInt();
+        int length = getInt();
+
+        std::stringstream loc;
+        loc << id << ".oga";
+
+        Xspf::XspfTrack track = Xspf::XspfTrack();
+        track.giveAlbum(album.c_str(), true);
+        track.giveCreator(artist.c_str(), true);
+        track.giveTitle(title.c_str(), true);
+        track.setTrackNum(tracknumber);
+        track.setDuration(length * 1000);
+        track.giveAppendLocation((std::string("http://129.241.138.184:9996/resource/") + loc.str()).c_str(), true);
+        writer->addTrack(track);
+    } while(step());
+
+    int bytes = 500000;
+    char *output = new char[bytes];
+    writer->writeMemory(output, bytes);
+    std::string out(output);
+    delete[] output;
+    delete writer;
+    return out;
+}
 std::string DBQuery::processQuery()
 {
     // TODO: Bad queries merely return string error instead of 504
     bool res = step();
-    std::cout << "error: " << res << std::endl;
 
     if(res) {
         scanColumns();
@@ -233,4 +352,3 @@ void DBQuery::scanColumns()
         columnTypes.push_back(ColType(i));
     }
 }
-
--- daemon/DBQuery.h
+++ daemon/DBQuery.h
@@ -20,9 +20,14 @@ class DBQuery : SQLite {
         void fetchAlbums();
         void fetchPaths();
         void fetchM3UAlbum(std::string album);
+        void fetchXSPFAlbum(std::string album);
         void fetchM3UArtist(std::string artist);
+        void fetchXSPFArtist(std::string artist);
+        void fetchXSPFDate(std::string date);
         void fetchM3URandom();
+        void fetchXSPFRandom();
         std::string processM3U();
+        std::string processXSPF();
         void fetchAlbum(std::string album);
         void fetchImages(std::string pathId);
         void scanColumns();
